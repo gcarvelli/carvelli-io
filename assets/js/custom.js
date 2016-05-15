a78e7f5550ca4d8173203464c91e039f2586af75
Giovanni Carvelli <giovannircarvelli@gmail.com>
2016-05-15 01:15:28 -0400

    more real text, real profile links

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,24 +4,65 @@ var app = angular.module('gcarvelliWebsite', [])
         $scope.profile = {
             title: "Hello, I'm Gio.",
             image: "http://gravatar.com/avatar/9c5c1a414e20e714b0ea466fa6617940",
-            intro: "I spent four years as a Software Engineering major at RIT but I plan on graduating Computer Science in May, 2017. "
+            intro: "I spent four years as a Software Engineering major at RIT but I plan on graduating Computer Science in May, 2017. ",
+            contact: "Want to get in touch? Send me a message on one of my social networks or use the form below!"
         };
 
-        $scope.icons = [
+        $scope.profiles = [
             {
                 name: "Facebook",
                 icon: "fa-facebook",
-                link: "https://facebook.com/gcarvelli"
+                link: "https://facebook.com/gcarvelli",
+                handle: "gcarvelli",
+                contact: true
             },
             {
                 name: "Github",
                 icon: "fa-github",
-                link: "https://github.com/gcarvelli"
+                link: "https://github.com/gcarvelli",
+                handle: "@gcarvelli",
+                contact: false
+            },
+            {
+                name: "LinkedIn",
+                icon: "fa-linkedin",
+                link: "https://linkedin.com/in/gcarvelli",
+                handle: "gcarvelli",
+                contact: true
             },
             {
                 name: "Email",
                 icon: "fa-envelope-o",
-                link: "mailto:gio@carvelli.io"
+                link: "mailto:gio@carvelli.io",
+                handle: "gio@carvelli.io",
+                contact: true
+            }
+        ];
+
+        $scope.projects = [
+            {
+                name: "House Explorer",
+                image: "images/fulls/01.jpg",
+                link: "https://github.com/gcarvelli/house-explorer",
+                description: "A text-based game engine written in Python."
+            },
+            {
+                name: "Geddit",
+                image: "images/fulls/02.jpg",
+                link: "https://github.com/gcarvelli/geddit",
+                description: "Reddit clone written from scratch in Ruby on Rails."
+            },
+            {
+                name: "Which Exchange",
+                image: "images/thumbs/03.jpg",
+                link: "https://github.com/WhichExchange/WhichExchange",
+                description: "Guess which StackExchange site each question comes from!"
+            },
+            {
+                name: "Community Care Hub",
+                image: "images/thumbs/04.jpg",
+                link: "https://github.com/amk5401/CommunityCareHub",
+                description: "A hastily-put-together HCI prototype for an elderly care home. Created for a Software Usability class."
             }
         ]
     });
