1b349c331f7c7db0fe56196f2fd5dc0abd143eee
Giovanni Carvelli <giovannircarvelli@gmail.com>
2016-08-01 14:25:50 -0700

    Change project picture locations, underline project text on hover

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -47,43 +47,43 @@ var app = angular.module('gcarvelliWebsite', [])
 
         $scope.projects = [
             {
-                name: "House Explorer",
-                image: $scope.config["asset_link"] + "/fulls/01.jpg",
-                link: "https://github.com/gcarvelli/house-explorer",
-                link_text: "View Source",
-                description: "A text-based game engine written in Python."
-            },
-            {
                 name: "Geddit",
-                image: $scope.config["asset_link"] + "/fulls/02.jpg",
+                image: $scope.config["asset_link"] + "/projects/02.jpg",
                 link: "https://github.com/gcarvelli/geddit",
                 link_text: "View Source",
                 description: "Reddit clone written from scratch in Ruby on Rails."
             },
             {
                 name: "Which Exchange",
-                image: $scope.config["asset_link"] + "/thumbs/03.jpg",
+                image: $scope.config["asset_link"] + "/projects/03.jpg",
                 link: "http://www.kwikthinkers.xyz/whichexchange/",
                 link_text: "View Demo",
                 description: "Guess which StackExchange site each question comes from! HackNJIT 2015 project."
             },
             {
+                name: "House Explorer",
+                image: $scope.config["asset_link"] + "/projects/house-explorer.png",
+                link: "https://github.com/gcarvelli/house-explorer",
+                link_text: "View Source",
+                description: "A text-based game engine written in Python."
+            },
+            {
                 name: "Reddit Backgrounds",
-                image: $scope.config["asset_link"] + "/thumbs/04.jpg",
+                image: $scope.config["asset_link"] + "/projects/04.jpg",
                 link: "https://github.com/chtinahow/reddit-backgrounds",
                 link_text: "View Source",
                 description: "Scrapes a list of subreddits for high resolution wallpapers. HackRPI 2015 project."
             },
             {
                 name: "Community Care Hub",
-                image: $scope.config["asset_link"] + "/thumbs/06.jpg",
+                image: $scope.config["asset_link"] + "/projects/06.jpg",
                 link: "https://github.com/amk5401/CommunityCareHub",
                 link_text: "View Source",
                 description: "A hastily-put-together HCI prototype for an elderly care home. Created for a Software Usability class."
             },
             {
                 name: "KwikThinker",
-                image: $scope.config["asset_link"] + "/thumbs/05.jpg",
+                image: $scope.config["asset_link"] + "/projects/05.jpg",
                 link: "https://github.com/KwikThinker/KwikThinker",
                 link_text: "View Source",
                 description: "Simple Android true/false trivia app. Award-winning HackRPI 2014 project."
